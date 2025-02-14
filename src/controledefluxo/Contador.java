f4ce801fd9001949f120986a8e4bfd2eb328d2b6
Breno Nogueira <brenonog14@gmail.com>
2025-02-14 09:04:31 -0300

    Comnetários adicionados

--- src/controledefluxo/Contador.java
+++ src/controledefluxo/Contador.java
@@ -6,12 +6,14 @@ public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
 
+        //Entradas do usuário para a realização da comparação
         System.out.println("Digite o primeiro parâmetro: ");
         int parametroUm = terminal.nextInt();
 
         System.out.println("Digite o segundo parâmetro: ");
         int parametroDois = terminal.nextInt();
 
+        //Local onde o método será utilizado
         try{
             contar(parametroUm, parametroDois);
         }catch(ParametrosInvalidosException e){
@@ -19,13 +21,14 @@ public class Contador {
         }
     }
 
+    //Método que irá validar as entradas do usuário
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         if(parametroUm > parametroDois){
             throw new ParametrosInvalidosException();
         }else if(parametroUm < parametroDois){
             int contagem = parametroDois - parametroUm;
             for(int i = 0; i <= contagem; i++){
-                System.out.println("imprimindo o número " + i);
+                System.out.println("Imprimindo o número " + i);
             }
         }
     }
